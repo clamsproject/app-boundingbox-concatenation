dda52aa53e930a1e2e2e9a3c9da86a37aeec45cf
Dean Cahill <dpcahill98@gmail.com>
2023-09-26 15:00:48 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Bounding Box Concatenation
+
+## User instruction
+
+General user instructions for CLAMS apps is available at [CLAMS Apps documentation](https://apps.clams.ai/clamsapp).
+
+Below is a list of additional information specific to this app.
+
+### System requirments
+
+No additional system requirements are necessary.
+
+### Configurable runtime parameter
+
+As the use of this app is relatively limited, there are no configurable runtime parameters for this app.
---
+++ metadata.py
@@ -0,0 +1,60 @@
+"""
+The purpose of this file is to define the metadata of the app with minimal imports. 
+
+DO NOT CHANGE the name of the file
+"""
+
+from mmif import DocumentTypes, AnnotationTypes
+
+from clams.app import ClamsApp
+from clams.appmetadata import AppMetadata
+
+
+# DO NOT CHANGE the function name 
+def appmetadata() -> AppMetadata:
+    """
+    Function to set app-metadata values and return it as an ``AppMetadata`` obj.
+    Read these documentations before changing the code below
+    - https://sdk.clams.ai/appmetadata.html metadata specification. 
+    - https://sdk.clams.ai/autodoc/clams.appmetadata.html python API
+    
+    :return: AppMetadata object holding all necessary information.
+    """
+    
+    # ================================|
+    # Metadata
+    metadata = AppMetadata(
+        name="BoundingBox Concatenation",
+        description="Converts a series of bounding-boxes at a given timepoint into a single bounding box.",
+        app_license="MIT",
+        identifier="app-boundingbox-concatenation",
+        url="https://github.com/clamsproject/app-boundingbox-concatenation"
+    )
+
+    # ================================|
+    # IO Spec
+    metadata.add_input(DocumentTypes.Document)
+    metadata.add_input(AnnotationTypes.BoundingBox, boxType='text')
+    metadata.add_input(AnnotationTypes.Alignment)
+    
+    metadata.add_output(AnnotationTypes.BoundingBox, boxType="text")
+
+    # ================================|
+    # Runtime Parameters
+    #TODO: Dean Cahill 09/26/23: should we give this app a frameType?
+    metadata.add_parameter(name="timeUnit",
+                           description="the division of time processing",
+                           type="string",
+                           choices=["frames","seconds","milliseconds"],
+                           default="frames")
+    
+    return metadata
+
+
+# DO NOT CHANGE the main block
+if __name__ == '__main__':
+    import sys
+    metadata = appmetadata()
+    for param in ClamsApp.universal_parameters:
+        metadata.add_parameter(**param)
+    sys.stdout.write(appmetadata().jsonify(pretty=True))
