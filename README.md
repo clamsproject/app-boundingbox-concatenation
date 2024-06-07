daac65ee33607af8703d120614128958d9b86de2
Dean Cahill <dpcahill98@gmail.com>
2024-06-07 11:24:42 -0400

    improved docstrings + readme for app publication

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Bounding Box Concatenation
 
+This app provides a utility for OCR bounding box concatenation. By concatenating all of the bounding boxes in a given frame, we
+can treat each frame as a single document for OCR - giving us much more flexibility in terms of reading order algorithm implementations.
+
+Within an OCR pipeline, this app ideally should be run between text detection (bounding box drawing) and the optical character recognition itself.
+
 ## User instruction
 
 General user instructions for CLAMS apps is available at [CLAMS Apps documentation](https://apps.clams.ai/clamsapp).
--- metadata.py
+++ metadata.py
@@ -10,17 +10,17 @@ from clams.app import ClamsApp
 from clams.appmetadata import AppMetadata
 
 
-# DO NOT CHANGE the function name 
+# DO NOT CHANGE the function name
 def appmetadata() -> AppMetadata:
     """
     Function to set app-metadata values and return it as an ``AppMetadata`` obj.
     Read these documentations before changing the code below
-    - https://sdk.clams.ai/appmetadata.html metadata specification. 
+    - https://sdk.clams.ai/appmetadata.html metadata specification.
     - https://sdk.clams.ai/autodoc/clams.appmetadata.html python API
-    
+
     :return: AppMetadata object holding all necessary information.
     """
-    
+
     # ================================|
     # Metadata
     metadata = AppMetadata(
@@ -28,7 +28,7 @@ def appmetadata() -> AppMetadata:
         description="Converts a series of bounding-boxes at a given timepoint into a single bounding box.",
         app_license="MIT",
         identifier="app-boundingbox-concatenation",
-        url="https://github.com/clamsproject/app-boundingbox-concatenation"
+        url="https://github.com/clamsproject/app-boundingbox-concatenation",
     )
 
     # ================================|
@@ -36,25 +36,30 @@ def appmetadata() -> AppMetadata:
     metadata.add_input(DocumentTypes.Document)
     metadata.add_input(AnnotationTypes.BoundingBox)
     metadata.add_input(AnnotationTypes.Alignment)
-    
     metadata.add_output(AnnotationTypes.BoundingBox)
 
     # ================================|
     # Runtime Parameters
-    metadata.add_parameter(name="timeUnit",
-                           description="the division of time processing",
-                           type="string",
-                           choices=["frames","seconds","milliseconds"],
-                           default="frames")
-    metadata.add_parameter(name="boxType",
-                           description="the type of boxes that are being concatenated",
-                           default="text")
+    metadata.add_parameter(
+        name="timeUnit",
+        description="the division of time processing",
+        type="string",
+        choices=["frames", "seconds", "milliseconds"],
+        default="frames",
+    )
+    metadata.add_parameter(
+        name="boxType",
+        description="the type of boxes that are being concatenated",
+        default="text",
+        type="string",
+    )
     return metadata
 
 
 # DO NOT CHANGE the main block
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
+
     metadata = appmetadata()
     for param in ClamsApp.universal_parameters:
         metadata.add_parameter(**param)
